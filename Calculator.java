dc22c92f6effad45cfe21e1db16a9b7cbcea2ee7
EliasKpc <s51820@edu.campus02.at>
2022-09-23 18:19:13 +0200

    Calculator with Variables

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        double Number1 = 1;
+        double Number2 = 2;
+        double ergebnis = Number1 + Number2;
+        String meinName = "Elias ";
+
+        System.out.println("Mein Name ist: "+ meinName + ergebnis);
+    }
+}
