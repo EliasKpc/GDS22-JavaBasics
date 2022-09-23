c5e7071c0ca683658590159841acbde6b9f9a20d
EliasKpc <s51820@edu.campus02.at>
2022-09-23 17:06:37 +0200

    First Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+
+    // Erste Klasse wurde erstellt
+
+    //psvm
+    public static void main(String[] args) {
+        // Erste Ausgabe
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        //args
+        //sout
+        System.out.println(Arrays.toString(args));
+    }
+}
