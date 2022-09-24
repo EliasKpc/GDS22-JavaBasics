9a34248dc1ed287dad923afedfc5c5023ee6333c
EliasKpc <s51820@edu.campus02.at>
2022-09-24 11:54:24 +0200

    Variables

---
+++ Variables.java
@@ -0,0 +1,34 @@
+public class Variables {
+
+    // Erstellung des Einstiegpunkts
+    public static void main(String[] args) {
+        //Variablen Deklaration
+        String firstName;
+        int x, y, z;
+
+        // Initialisierung
+        firstName = "Elias";
+
+        x = 1;
+        y = 2;
+        z = 3;
+
+        //Deklaration + Init
+        char a = 'a';
+        int p = 2, q = 3;
+
+        // variablen wert tauschen
+        firstName = "Max";
+
+        // Wert kann nicht nochal deklariert werden
+        //String firstName = "Franz";
+
+        // anderer Datentyp --> Fehler dadurch nicht möglich
+        // firstName = 1;
+
+        var firstName2 = firstName;
+        var doubleNr = 4000;
+
+
+    }
+}
